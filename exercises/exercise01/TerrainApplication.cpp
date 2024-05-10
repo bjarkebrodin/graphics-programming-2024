5e404c568b6151002ada55861a647f566c04d18c
Bjarke Brodin <bjarke@brodin.dk>
2024-05-10 19:03:33 +0200

    meh

--- exercises/exercise01/TerrainApplication.cpp
+++ exercises/exercise01/TerrainApplication.cpp
@@ -8,6 +8,8 @@
 
 
 
+
+
 // Helper structures. Declared here only for this exercise
 struct Vector2
 {
@@ -24,13 +26,17 @@ struct Vector3
 
     Vector3 Normalize() const
     {
-        float length = std::sqrt(1 + x * x + y * y);
+        float length = std::sqrt(1 + x * x + y * y + z * z);
         return Vector3(x / length, y / length, z / length);
     }
 };
 
-// (todo) 01.8: Declare an struct with the vertex format
-
+struct Vertex {
+    Vector3 position;
+    Vector2 texCoords;
+    Vector3 color;
+    Vector3 normal;
+};
 
 
 TerrainApplication::TerrainApplication()
@@ -47,18 +53,20 @@ void TerrainApplication::Initialize()
 
     glEnable(GL_DEPTH_TEST);
 
-    std::vector<Vector3> vertices;
-    std::vector<Vector2> texCoords;
+    std::vector<Vertex> vertices;
 	std::vector<unsigned int> indices;
-    std::vector<Vector3> colors;
+
+
 
     vertices.reserve((m_gridX+1) * (m_gridY+1));
-    texCoords.reserve((m_gridX + 1) * (m_gridY + 1));
-    colors.reserve((m_gridX + 1) * (m_gridY + 1));
     indices.reserve((m_gridX+1) * (m_gridY+1) * 6);
 
     for (unsigned int j = 0u; j < m_gridY + 1u; j++) {
         for (unsigned int i = 0u; i < m_gridX + 1u; i++) {
+            Vector3 position;
+            Vector2 texCoord;
+            Vector3 color;
+
             float x = i * (1.0f / m_gridX) - 0.5f;
             float y = j * (1.0 / m_gridY) - 0.5f;
             float z = 0.0f;
@@ -67,8 +75,8 @@ void TerrainApplication::Initialize()
             float gain = 0.5f;
             z = stb_perlin_fbm_noise3(x, y, z, lacunarity, gain, octaves);
 
-            vertices.emplace_back(x, y, z);
-            texCoords.emplace_back(static_cast<float>(i), static_cast<float>(j));
+            position = Vector3(x, y, z);
+            texCoord = Vector2(static_cast<float>(i), static_cast<float>(j));
 
             Vector3 water = Vector3(0.0f, 0.2f, 1.0f);
             Vector3 soil = Vector3(0.8f, 0.5f, 0.4f);
@@ -76,17 +84,42 @@ void TerrainApplication::Initialize()
             Vector3 snow = Vector3(1.0f, 1.0f, 1.0f);
 
             if (z > 0.5f) {
-                colors.emplace_back(snow);
+                color = snow;
             }
             else if (z > 0.0f) {
-                colors.emplace_back(rock);
+                color = rock;
             }
             else if (z > -0.5f) {
-                colors.emplace_back(soil);
+                color = soil;
             }
             else {
-                colors.emplace_back(water);
+                color = water;
             }
+
+            vertices.emplace_back(position, texCoord, color, Vector3(0.f,0.f,0.f));
+        }
+    }
+
+    for (unsigned int j = 1u; j < m_gridY; j++) {
+        for (unsigned int i = 1u; i < m_gridX; i++) {
+            unsigned int ihere = j * m_gridX + i;
+            unsigned int iup = ihere + m_gridX;
+            unsigned int idown = ihere - m_gridX;
+            unsigned int ileft = ihere - 1u;
+            unsigned int iright = ihere + 1u;
+
+            auto here = vertices[ihere];
+            auto up = vertices[iup];
+            auto down = vertices[idown];
+            auto left = vertices[ileft];
+            auto right = vertices[iright];
+
+
+            float dx = (right.position.z - left.position.z) / (right.position.x - left.position.x);
+            float dy = (up.position.z - down.position.z) / (up.position.x - down.position.x);
+
+            vertices[ihere].normal = Vector3(dx, dy, 1.0f).Normalize();
+           
         }
     }
 
@@ -111,28 +144,21 @@ void TerrainApplication::Initialize()
     vao.Bind();
     vbo.Bind();
 
+
     VertexAttribute position(Data::Type::Float, 3);
     VertexAttribute textureCoordinate(Data::Type::Float, 2);
     VertexAttribute color(Data::Type::Float, 3);
+    VertexAttribute normal(Data::Type::Float, 3);
 
-    unsigned int posOffset = 0;
-    unsigned int texOffset = posOffset + vertices.size() * position.GetSize();
-    unsigned int colOffset = texOffset + vertices.size() * textureCoordinate.GetSize();
-
-    unsigned int vSize = position.GetSize() + textureCoordinate.GetSize() + color.GetSize();
-    unsigned int vboBytes = vertices.size() * vSize;
-
-    vbo.AllocateData(vboBytes);
-    vbo.UpdateData<Vector3>(vertices, 0);
-    vbo.UpdateData<Vector2>(texCoords, texOffset);
-    vbo.UpdateData<Vector3>(colors, colOffset);
+    vbo.AllocateData<Vertex>(vertices);
 
     ebo.Bind();
     ebo.AllocateData<unsigned int>(indices);
 
-    vao.SetAttribute(0, position, posOffset, 0);
-    vao.SetAttribute(1, textureCoordinate, texOffset, 0);
-    vao.SetAttribute(2, color, colOffset, 0);
+    vao.SetAttribute(0, position, 0, sizeof(Vertex));
+    vao.SetAttribute(1, textureCoordinate, position.GetSize(), sizeof(Vertex));
+    vao.SetAttribute(2, color, position.GetSize() + textureCoordinate.GetSize(), sizeof(Vertex));
+    vao.SetAttribute(3, normal, position.GetSize() + textureCoordinate.GetSize() + color.GetSize(), sizeof(Vertex));
 
     vao.Unbind();
     vbo.Unbind();
